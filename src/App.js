3548cfebc6321af05d96ca2e79d368d56b12e589
BradleyHollis <bhollis@dev-10.com>
2023-02-03 12:48:07 -0500

    Create and delete methods added to App.js

--- src/App.js
+++ src/App.js
@@ -8,14 +8,23 @@ function App() {
   const [recipes, setRecipes] = useState(RecipeData);
 
   // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
-  // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
+  const deleteRecipe = (indexToDelete) =>
+  setRecipes((currentRecipes) =>
+    currentRecipes.filter((recipe, index) => index !== indexToDelete)
+  );
 
+  // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
+  const createRecipe = (newRecipe) =>
+  setRecipes((currentRecipes) => [
+    newRecipe,
+    ...currentRecipes,
+  ]);
   
   return (
     <div className="App">
       <header><h1>Delicious Food Recipes</h1></header>
-      <RecipeList />
-      <RecipeCreate />
+      <RecipeList deleteRecipe={deleteRecipe}/>
+      <RecipeCreate createRecipe={createRecipe}/>
     </div>
   );
 }
